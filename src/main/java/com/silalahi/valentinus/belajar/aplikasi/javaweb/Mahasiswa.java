dd4cafd384b644033659da24b1b17fdabc62fa71
valentinus <valentinus@voxteneo.co.id>
2016-08-28 21:32:22 +0700

    domain object utk menampung data

---
+++ src/main/java/com/silalahi/valentinus/belajar/aplikasi/javaweb/Mahasiswa.java
@@ -0,0 +1,71 @@
+package com.silalahi.valentinus.belajar.aplikasi.javaweb;
+
+import java.util.Date;
+
+public class Mahasiswa {
+
+    private Integer id;
+    private String nim;
+    private String nama;
+    private String tempatLahir;
+    private Date tanggalLahir;
+    private JenisKelamin jenisKelamin; //menggunakan enum
+    private String alamat;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public JenisKelamin getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(JenisKelamin jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+}
